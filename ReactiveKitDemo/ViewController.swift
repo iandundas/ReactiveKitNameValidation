caba57b157173de711d7978ee45b3f8bc3323b0e
Ian Dundas <contact@iandundas.co.uk>
2016-01-09 17:24:26 +0100

    basic ReactiveKit demo

--- ReactiveKitDemo/ViewController.swift
+++ ReactiveKitDemo/ViewController.swift
@@ -8,21 +8,37 @@ import ReactiveUIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var indicator: UISwitch!
+    @IBOutlet weak var switchIndicator: UISwitch!
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet var errorBox: UIView!
     
     let viewModel = ViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Bind the controls to the ViewModel:
-        textField.rText.bindTo(viewModel.name)
+        /* ~~ 1: Bind the controls to the ViewModel: ~~ */
+        textField.rText.bindTo(viewModel.nameString)
+        button.rTap.bindTo(viewModel.submitEventStream) // button `taps` are sent to the viewModel
         
-        // Bind the ViewModel to the controls:
-        viewModel.nameValidation.bindTo(label.rText)
-        viewModel.nameIsValid.bindTo(indicator.rOn)
+        
+        /* ~~ 2: Bind the ViewModel to the controls: ~~ */
+        viewModel.nameIsValidBool.bindTo(switchIndicator.rOn) // switch goes on/off depending on nameIsValid
+        
+        // error string shown in Label:
+        viewModel.nameValidationString.bindTo(label.rText)
+        
+        // label is hidden or shown based on `shouldShowErrorIndicators`
+        viewModel.shouldShowErrorIndicators
+            .map{should in return !should} // true becomes false, false becomes true..
+            .bindTo(label.rHidden)
+
+        // errorBox has background set to Red or White depending on validity (just to demo something other than .hidden :P) 
+        viewModel.shouldShowErrorIndicators
+            // Map Boolean value to a UIColor value:
+            .map{ should in return (should ? UIColor.redColor() : UIColor.whiteColor())}
+            .bindTo(errorBox.rBackgroundColor)
     }
 }
 
--- ReactiveKitDemo/ViewModel.swift
+++ ReactiveKitDemo/ViewModel.swift
@@ -6,24 +6,42 @@ import ReactiveKit
 
 class ViewModel{
     
-    // Mutable Observable properties:
-    var name: Observable<String?>
+    /*  An <ActiveStream> simply streams events as they happen (i.e. new values) from the moment you call .observeNext on it. 
+        Any values that occured *before* you *observed* it are forgotten.
+        Therefore they have a memory of 0 items.
     
-    // Derived properties:
-    var nameValidation: Stream<String?>
-    var nameIsValid: Stream<Bool>
+        It sends values regardless of whether it has any subscribers.
+    */
+    var submitEventStream = ActiveStream<Void>()
+    
+    /*  An <Observable> is a kind of Active Stream, but it buffers the last value, so when we observe it, we get that value immediately.
+        There is also a method `.value` on an Observable which hands us immediately the latest value, which is very convenient.
+        They have a memory of 1 item.
+        NB this is called a Signal in Interstellar.
+        This is basically like a variable that emits "value did change" events.
+        */
+    
+    var nameString: Observable<String?>
+    var shouldShowErrorIndicators = Observable<Bool>(false)
+    
+    /*  <Stream> types lie dormant doing nothing until they are observed. It defers all work and the sending of any 
+        values until it has an observer. We can't set the value ourself.*/
+    var nameValidationString: Stream<String?>
+    var nameIsValidBool: Stream<Bool>
     
     init(){
-        name = Observable<String?>(nil)
+        nameString = Observable<String?>(nil)
         
-        nameValidation = name.map { potentialName -> String? in
-            guard let name = potentialName else { return "please provide a name" }
+        nameValidationString = nameString.map { potentialName -> String? in
+            guard let name = potentialName where name.characters.count > 0 else { return "Please provide a name" }
             return name.characters.count >= 3 ? nil : "Name should contain at least 3 characters"
         }
 
-        nameIsValid = nameValidation.map{ errorMessage in (errorMessage == nil)}
-        
-        
+        nameIsValidBool = nameValidationString.map{ errorMessage in (errorMessage == nil)}
+
+        let combineSubmitEventWithNameIsValid = submitEventStream.combineLatestWith(nameIsValidBool.map{!$0})
+
+        // map `combineSubmitEventWithNameIsValid` to only the `isInvalid` bit of the tuple, return it and bind that to shouldShowErrorIndicators
+        combineSubmitEventWithNameIsValid.map{_, isInvalid in isInvalid}.bindTo(shouldShowErrorIndicators)
     }
-    
 }
